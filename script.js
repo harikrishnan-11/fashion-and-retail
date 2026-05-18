209a17da1fbf2e38647cbb39097329cddad3a52d
NirmalkumarS2002 <nirmalsiva555@gmail.com>
2026-05-18 16:00:06 +0530

    js created

--- script.js
+++ script.js
@@ -5,3 +5,27 @@
     menuBtn.addEventListener("click", () => {
       navLinks.classList.toggle("active");
     });
+
+    // Hero section
+
+    let hero=document.querySelector(".hero")
+    let image=["./src/home.webp","./src/home1.png"]
+    let clslist=document.querySelectorAll(".hero-content")
+    let index=0
+
+    setInterval(()=>{
+        index++
+        if(index>=image.length){
+            index=0
+        }
+        hero.style.backgroundImage=`url(${image[index]})`
+
+       
+    clslist.forEach((item) => {
+        item.classList.remove("active")
+    })
+
+    clslist[index].classList.add("active")
+
+    },3000)
+
